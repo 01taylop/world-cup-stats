c3f6999971611e44422034fa1a3285e1ec17b916
Patrick Taylor <patchfalcon+github@gmail.com>
2019-06-16 19:09:32 +0100

    Updates

---
+++ precache-manifest.9fa43c0f12bf3eff821ddd6ce2774341.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92765a154f30e211dff6497a4cd27256",
+    "url": "/world-cup-stats/index.html"
+  },
+  {
+    "revision": "5a2d238fc42e6a858b5e",
+    "url": "/world-cup-stats/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/world-cup-stats/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "5a2d238fc42e6a858b5e",
+    "url": "/world-cup-stats/static/js/main.bafa9f26.chunk.js"
+  },
+  {
+    "revision": "4a2e76af80dd37ff6e58",
+    "url": "/world-cup-stats/static/js/runtime~main.5163323f.js"
+  }
+]);
