1de05bf43e346339bf07de50a52110020ee4e557
Patrick Taylor <patchfalcon+github@gmail.com>
2019-06-23 23:45:14 +0100

    Added Sidebar items 🦉

--- src/js/apps/CoreApp/index.js
+++ src/js/apps/CoreApp/index.js
@@ -4,6 +4,12 @@ import AppBar from '../../components/AppBar'
 import Flag from '../../components/Flag'
 import Sidebar from '../../components/Sidebar'
 
+import HomeIcon from '@material-ui/icons/Home'
+import TableIcon from '@material-ui/icons/FormatListNumbered'
+import TeamIcon from '@material-ui/icons/Group'
+import PlayersIcon from '@material-ui/icons/Person'
+import SettingsIcon from '@material-ui/icons/Settings'
+
 export default class App extends React.Component {
 
   state = {
@@ -23,7 +29,12 @@ export default class App extends React.Component {
       <>
         <AppBar title='World Cup Stats' onToggleMenu={this.toggleMenu}/>
         <Sidebar open={isMenuOpen}>
-          <p>Hello</p>
+          <a href='/'><HomeIcon/>Home</a>
+          <a href='/'><TableIcon/>Table</a>
+          <a href='/'><TeamIcon/>Teams</a>
+          <a href='/'><PlayersIcon/>Players</a>
+          <hr/>
+          <a href='/'><SettingsIcon/>Settings</a>
         </Sidebar>
         <div className='content'>
           <p>This is my app</p>
